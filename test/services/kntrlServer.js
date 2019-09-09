8d05158a75adb5263db397251c5b62d78ef2ef92
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2019-09-09 17:02:49 +0100

    [update] optimized journal log seearch

--- test/services/kntrlServer.js
+++ test/services/kntrlServer.js
@@ -5,7 +5,6 @@ const kntrlServer = require('../../src/services/kntrlServer')
 
 it('Return Log from fail2banexample file ', () => {
     const serverLog = new kntrlServer()
-    serverLog.init()
-    
+    console.log(serverLog.init());
     // assert.isTrue(serverLog.init(), true)
 });
